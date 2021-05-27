5f58aab7e9803b411e0a07bc8452c1586efb1325
tincho77 <rosas.martin.exequiel@gmail.com>
2021-05-27 20:20:26 -0300

    implementation of provider for theme selector

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_flutter/blocs/theme.dart';
 import 'package:provider_flutter/pages/home.dart';
 
 void main() {
@@ -6,10 +8,21 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => ThemeChanger(ThemeData.dark()),
+      child: MaterialAppWithTheme(),
+    );
+  }
+}
+
+class MaterialAppWithTheme extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeChanger>(context);
     return MaterialApp(
+      theme: theme.getTheme(),
       home: HomePage(),
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_flutter/blocs/theme.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeChanger>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('AppBar'),
       ),
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
+      body: ListaBotones(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => theme.SetTheme(ThemeData(
+            brightness: Brightness.light,
+            primaryColor: Colors.lime,
+            floatingActionButtonTheme:
+                FloatingActionButtonThemeData(backgroundColor: Colors.lime))),
       ),
+    );
+  }
+}
+
+class ListaBotones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeChanger>(context);
+    return Column(
+      children: <Widget>[
+        TextButton(
+            child: Text('Ligth'),
+            onPressed: () => theme.SetTheme(ThemeData.light())),
+        TextButton(
+            child: Text('Dark'),
+            onPressed: () => theme.SetTheme(ThemeData.dark())),
+      ],
     );
   }
 }
